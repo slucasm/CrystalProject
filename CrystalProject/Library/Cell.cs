40b34776d274d8a5d8d42df2f258114af725e433
slucasm <sergilucasmillan@hotmail.com>
2018-10-11 10:48:23 +0200

    First commit

---
+++ CrystalProject/Library/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    class Cell
+    {
+        int phase_actual;
+        int temperature_actual;
+
+
+        public Cell(int phase_actual,int temperature_actual)
+        {
+            this.phase_actual = phase_actual;
+            this.temperature_actual = temperature_actual;
+        }
+
+        public int getTemperature()
+        {
+            return this.temperature_actual;
+        }
+
+        public int getPhase()
+        {
+            return this.phase_actual;
+        }
+
+
+    }
+}
